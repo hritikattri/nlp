30021b4644ee9cf126f02186b4b8e2e82d2d7009
HritikAttri <hritikattri10@gmail.com>
2019-11-23 14:23:16 +0530

    Updating files

---
+++ sentiment_analysis/app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+api = Api(app)
+
+with open('vectorizer.pkl', 'rb') as f:
+	vectorizer = pickle.load(f)
+
+with open('sentiment_classifier.pkl', 'rb') as f:
+	sentiment_classifier = pickle.load(f)
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+class PredictSentiment(Resource):
+	def get(self):
+		args = parser.parse_args()
+		user_query = args['query']
+
+		user_input = vectorizer.transform([str(user_query)])
+		prediction = sentiment_classifier.predict(user_input)
+
+		result = 'Negative' if prediction == 0 else 'Positive'
+		output = {'Prediction': result}
+
+		return output
+
+api.add_resource(PredictSentiment, '/')
+
+if __name__ == '__main__':
+	app.run(debug = True)
+
